f05eac496ede5697c91373cf496dd21e87932e51
Charlie Denhart <cccdenhart@icloud.com>
2024-08-19 09:22:03 -0700

    Add streamlit app

---
+++ README.md
@@ -0,0 +1,20 @@
+# Transmission Capacity
+
+This project is a web app that is designed to help energy generation project managers plan for where is best to implement new generation projects.  The app provides several variables to investigate with including:
+- existing transmission capacity
+- congestion costs
+- existing solar/wind generation
+
+## Setup
+
+### Large Files
+
+Follow the [instructions here](https://git-lfs.com/) to ensure git LFS is installed to properly interact with data files via git.
+
+### Dependencies
+`pip install -r requirements.txt`
+
+## Run the app
+
+`python -m streamlit run app.py`
+
---
+++ app.py
@@ -0,0 +1,67 @@
+import json
+import os
+
+import branca.colormap as cm
+import folium
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import streamlit as st
+from dotenv import load_dotenv
+from streamlit_folium import st_folium
+
+load_dotenv()
+
+@st.cache_data
+def load_data() -> pd.DataFrame:
+    transmission_fp = "data/pjm_transmission.geojson"
+    with open(transmission_fp) as f:
+        gj = json.load(f)
+
+    geo_df = gpd.GeoDataFrame.from_features(gj["features"])
+
+    cost_fp = "data/pjm_july_lmp.csv"
+    cost_df = pd.read_csv(cost_fp)
+
+    geo_cost_df = geo_df.merge(cost_df, left_on="SUB_1", right_on="pnode_name", how="inner")
+    avg_geo_cost_df = (
+        geo_cost_df.groupby(
+            ["ID", "pnode_id", "pnode_name", "SUB_1", "SUB_2", "VOLTAGE", "geometry"]
+        )
+        .mean("congestion_price_rt")
+        .reset_index()
+    )
+
+    return avg_geo_cost_df
+
+# Base page info
+st.title("Energy Generation Planning App")
+
+layer = st.selectbox("Feature", [None, "Congestion Cost"])
+
+# Load data
+df = load_data()
+
+# Initialize a Folium map
+m = folium.Map(location=[39.653806, -77.152707], zoom_start=7)
+
+# Add the colormap legend to the map
+costs = df["congestion_price_da"]
+min_value, max_value = min(costs), max(costs)
+colormap = cm.linear.YlOrRd_09.scale(min_value, max_value)
+colormap.add_to(m)
+
+for _, row in df.iterrows():
+    linestrings = row["geometry"]
+    name = row["ID"]
+    cost = row["congestion_price_da"]
+
+    for linestring in linestrings.geoms:
+        locations = np.flip(np.stack(linestring.xy, axis=1), axis=1)
+        if layer == "Congestion Cost":
+            color = colormap(cost)  # Get color based on value
+        else:
+            color = colormap(min_value)
+        folium.PolyLine(locations=locations, color=color, weight=5).add_to(m)
+
+st_data = st_folium(m, width=725)
