3d7e5fb951a465d77554c2b00b788fd82246dbb5
valentino-christian <val.c.christian@gmail.com>
2024-08-20 22:33:32 -0700

    added substation location, removed for loop

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import pandas as pd
 import streamlit as st
 from dotenv import load_dotenv
 from streamlit_folium import st_folium
+from folium.plugins import MarkerCluster
 
 load_dotenv()
 
@@ -36,6 +37,18 @@ def load_data() -> pd.DataFrame:
 
     return avg_geo_cost_df
 
+@st.cache_data
+def load_substation_data() -> gpd.GeoDataFrame:
+    substation_fp = "data/Substations.csv"
+    substation_df = pd.read_csv(substation_fp)
+    substation_gdf = gpd.GeoDataFrame(
+        substation_df, geometry=gpd.points_from_xy(substation_df.LONGITUDE, substation_df.LATITUDE), crs="EPSG:4326"
+    )
+
+    return substation_gdf
+
+
+
 def get_colormap(values: List[float]) -> cm.ColorMap:
     min_value, max_value = min(values), max(values)
     colormap = cm.linear.YlOrRd_09.scale(min_value, max_value)
@@ -49,8 +62,11 @@ layer = st.selectbox("Feature", [None, "Congestion Cost", "Capacity"])
 # Load data
 df = load_data()
 
+# Load substation data
+substation_gdf = load_substation_data()
+
 # Initialize a Folium map
-m = folium.Map(location=[39.653806, -77.152707], zoom_start=7)
+m = folium.Map(location=[39.653806, -77.152707], zoom_start=7, prefer_canvas=True,)
 
 # Add the colormap legend to the map
 if layer == "Congestion Cost":
@@ -64,58 +80,22 @@ if color_values:
     colormap = get_colormap(color_values)
     colormap.add_to(m)
 
-for _, row in df.iterrows():
-    linestrings = row["geometry"]
-    name = row["ID"]
-    cost = row["congestion_price_rt"]
-    volts = row["VOLTAGE"]
-    sub1 = row["SUB_1"]
-    sub2 = row["SUB_2"]
+gdf = gpd.GeoDataFrame(df , geometry = df.geometry)
+gdf.crs="EPSG:4326"
 
-    for i, linestring in enumerate(linestrings.geoms):
-        locations = np.flip(np.stack(linestring.xy, axis=1), axis=1)
+folium.Choropleth(
+    gdf,
+    line_weight=3,
+    line_color='blue'
+).add_to(m)
 
-        if layer == "Congestion Cost":
-            color = colormap(cost)  # Get color based on value
-            layer_val = f"${round(cost, 2)}"
-        elif layer == "Capacity":
-            color = colormap(volts)
-            layer_val = f"{volts} kV"
-        else:
-            color = "blue"
-            layer_val = None
+marker_cluster = MarkerCluster(
+    locations=substation_gdf[['LATITUDE', 'LONGITUDE']].values.tolist(),
+    name="transmission network substations",
+    overlay=True,
+    control=True,
+)
 
-        line = folium.PolyLine(locations=locations, color=color, weight=5)
-
-        if layer_val:
-            tooltip = folium.Tooltip(f"{layer} = {layer_val}")
-            line.add_child(tooltip)
-            line.add_to(m)
-
-        # Add a substation point to the start of the PolyLine if it is the first line
-        if i == 0:
-            start_point = folium.CircleMarker(
-                location=locations[0],  # Start point coordinates
-                radius=3,
-                color='green',
-                fill=True,
-                fill_color='green',
-                fill_opacity=0.7,
-                tooltip=sub1
-            )
-            start_point.add_to(m)
-
-        # Add a substation point at the end of the PolyLine if it is the last line
-        if i == (len(linestrings.geoms) - 1):
-            end_point = folium.CircleMarker(
-                location=locations[-1],  # End point coordinates
-                radius=3,
-                color='green',
-                fill=True,
-                fill_color='green',
-                fill_opacity=0.7,
-                tooltip=sub2
-            )
-            end_point.add_to(m)
+marker_cluster.add_to(m)
 
 st_data = st_folium(m, width=725)
