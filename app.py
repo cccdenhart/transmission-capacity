abe1ffc98ecdf488c7bc03629fbe921335ee6027
Charlie Denhart <cccdenhart@icloud.com>
2024-08-21 22:02:16 -0700

    Connect congestion cost api data

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 import os
-from typing import List, Tuple
+from datetime import date, datetime, timedelta
+from typing import List, Optional, Tuple
 
 import branca.colormap as cm
 import folium
@@ -8,34 +9,129 @@ import geopandas as gpd
 import numpy as np
 import pandas as pd
 import rasterio
+import requests
 import streamlit as st
 from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
 from streamlit_folium import st_folium
 
 load_dotenv()
 
 @st.cache_data
-def load_data() -> pd.DataFrame:
-    transmission_fp = "data/pjm_transmission.geojson"
+def load_base_data() -> pd.DataFrame:
+    transmission_fp = "data/ne_iso_transmission.geojson"
     with open(transmission_fp) as f:
         gj = json.load(f)
 
     geo_df = gpd.GeoDataFrame.from_features(gj["features"])
+    geo_df = geo_df.loc[geo_df["VOLTAGE"] > 0]
 
-    cost_fp = "data/pjm_july_lmp.csv"
-    cost_df = pd.read_csv(cost_fp)
+    pnodes_fp = "data/ne_iso_pnodes.csv"
+    pnodes_df = pd.read_csv(pnodes_fp)
 
-    geo_cost_df = geo_df.merge(cost_df, left_on="SUB_1", right_on="pnode_name", how="inner")
-    avg_geo_cost_df = (
-        geo_cost_df.groupby(
-            ["ID", "pnode_id", "pnode_name", "SUB_1", "SUB_2", "VOLTAGE", "geometry"]
-        )
-        .mean(["congestion_price_rt", "VOLTAGE"])
-        .reset_index()
-    )
-    avg_geo_cost_df = avg_geo_cost_df.loc[avg_geo_cost_df["VOLTAGE"] > 0]
+    base_df = geo_df.merge(pnodes_df, left_on="SUB_1", right_on="Substation Long Name", how="inner")
+    base_df["Node/Unit ID"] = base_df["Node/Unit ID"].astype(int)
 
-    return avg_geo_cost_df
+    return base_df
+
+@st.cache_data
+def query_ne_iso_api(endpoint: str) -> dict:
+    base_url = 'https://webservices.iso-ne.com/api/v1.1'
+    url = base_url + endpoint
+    username = os.getenv("NE_ISO_EMAIL")
+    password = os.getenv("NE_ISO_PASSWORD")
+    headers = {
+        'Accept': 'application/json'
+    }
+
+    # Make the GET request with basic authentication
+    response = requests.get(url, auth=HTTPBasicAuth(username, password), headers=headers)
+
+    if not response.status_code == 200:
+        print(f"Failed with status code: {response.status_code}")
+        print(response.content)
+        return {}
+
+    return response.json()
+
+
+@st.cache_data
+def query_congestion(day: Optional[date]) -> pd.DataFrame:
+    if day:
+        day_str = day.strftime("%Y%m%d")
+    else:
+        day_str = "current"
+
+    endpoint = f"/hourlylmp/rt/final/day/{day_str}"
+    data = query_ne_iso_api(endpoint)
+
+    if data == {}:
+        st.warning("No data returned for congestion cost data.")
+        return pd.DataFrame()
+
+    rows = []
+    for record in data["HourlyLmps"]["HourlyLmp"]:
+        row = {
+            "date": record["BeginDate"],
+            "loc_id": record["Location"]["@LocId"],
+            "lmp": record["LmpTotal"],
+            "congestion": record["CongestionComponent"],
+            "energy": record["EnergyComponent"],
+            "loss": record["LossComponent"]
+        }
+        rows.append(row)
+
+    lmp_df = pd.DataFrame(rows)
+    lmp_df["date"] = pd.to_datetime(lmp_df["date"])
+    lmp_df["loc_id"] = lmp_df["loc_id"].astype(int)
+    day_lmp_df = lmp_df.groupby([pd.Grouper(key='date', freq='D'), lmp_df.loc_id]).mean().reset_index()
+
+    return day_lmp_df
+
+@st.cache_data
+def query_capability(day: Optional[date]) -> pd.DataFrame:
+    if day:
+        day_str = day.strftime("%Y%m%d")
+    else:
+        day_str = "current"
+
+    endpoint = f"/totaltransfercapability/day/{day_str}"
+    data = query_ne_iso_api(endpoint)
+
+    if data == {}:
+        st.warning("No data returned for capability data.")
+        return pd.DataFrame()
+
+    rows = []
+    for record in data["Ttcs"]["Ttc"]:
+        for location in record["TtcLocations"]["TtcLocation"]:
+            row = {
+                "date": record["BeginDate"],
+                "loc_id": location["Location"]["@LocId"],
+                "import_mw": location["ImportLimitMw"],
+                "export_mw": location["ExportLimitMw"]
+            }
+            rows.append(row)
+
+    ttc_df = pd.DataFrame(rows)
+    ttc_df["date"] = pd.to_datetime(ttc_df["date"])
+    ttc_df["loc_id"] = ttc_df["loc_id"].astype(int)
+
+    return ttc_df
+
+@st.cache_data
+def merge_api_data(layer: str, input_date: date) -> pd.DataFrame:
+    global base_df
+
+    if layer == "Congestion Cost":
+        api_df = query_congestion(input_date)
+    else:
+        return base_df
+    
+    df = api_df.merge(base_df, left_on="loc_id", right_on="Node/Unit ID", how="inner")
+
+    return df
+
 
 def get_colormap(values: List[float]) -> cm.ColorMap:
     min_value, max_value = min(values), max(values)
@@ -51,20 +147,39 @@ def load_solar_tif() -> Tuple[np.ndarray, List[Tuple[float, float]]]:
     bbox = [(bounds.bottom, bounds.left), (bounds.top, bounds.right)]
     return array, bbox
 
+def in_region(x: int, y: int) -> bool:
+    top_left = [45.085031, -73.539937]
+    bottom_right = [41.205254, -70.313054]
+    return (
+        y > top_left[1] and 
+        y < bottom_right[1] and
+        x < top_left[0] and
+        x > bottom_right[0]
+    )
+
 # Base page info
 st.title("Energy Generation Planning App")
 
+# Load static data
+base_df = load_base_data()
+
 layer = st.selectbox("Feature", [None, "Congestion Cost", "Capacity"])
 
-# Load data
-df = load_data()
+if layer == "Congestion Cost":
+    yesterday = datetime.now() - timedelta(days=1)
+    input_date = st.date_input("Date", value=yesterday)
+else:
+    input_date = None
+
+# Load api data
+df = merge_api_data(layer, input_date)
 
 # Initialize a Folium map
-m = folium.Map(location=[39.653806, -77.152707], zoom_start=7)
+m = folium.Map(location=[42.140311, -72.604366], zoom_start=7, prefer_canvas=True)
 
 # Add the colormap legend to the map
 if layer == "Congestion Cost":
-    color_values = df["congestion_price_da"].tolist()
+    color_values = df["lmp"].tolist()
 elif layer == "Capacity":
     color_values = df["VOLTAGE"].tolist()
 else:
@@ -77,55 +192,57 @@ if color_values:
 for _, row in df.iterrows():
     linestrings = row["geometry"]
     name = row["ID"]
-    cost = row["congestion_price_rt"]
-    volts = row["VOLTAGE"]
     sub1 = row["SUB_1"]
     sub2 = row["SUB_2"]
+    volts = row["VOLTAGE"]
 
     for i, linestring in enumerate(linestrings.geoms):
         locations = np.flip(np.stack(linestring.xy, axis=1), axis=1)
 
-        if layer == "Congestion Cost":
-            color = colormap(cost)  # Get color based on value
-            layer_val = f"${round(cost, 2)}"
-        elif layer == "Capacity":
-            color = colormap(volts)
-            layer_val = f"{volts} kV"
-        else:
-            color = "blue"
-            layer_val = None
+        if in_region(*locations[0]):
 
-        line = folium.PolyLine(locations=locations, color=color, weight=5)
+            if layer == "Congestion Cost":
+                cost = row["lmp"]
+                color = colormap(cost)  # Get color based on value
+                layer_val = f"${round(cost, 2)}"
+            elif layer == "Capacity":
+                color = colormap(volts)
+                layer_val = f"{volts} kV"
+            else:
+                color = "blue"
+                layer_val = None
 
-        if layer_val:
-            tooltip = folium.Tooltip(f"{layer} = {layer_val}")
-            line.add_child(tooltip)
+            line = folium.PolyLine(locations=locations, color=color, weight=3)
+
+            if layer_val:
+                tooltip = folium.Tooltip(f"{layer} = {layer_val}")
+                line.add_child(tooltip)
+
             line.add_to(m)
 
-        # Add a substation point to the start of the PolyLine if it is the first line
-        if i == 0:
-            start_point = folium.CircleMarker(
-                location=locations[0],  # Start point coordinates
-                radius=3,
-                color='green',
-                fill=True,
-                fill_color='green',
-                fill_opacity=0.7,
-                tooltip=sub1
-            )
-            start_point.add_to(m)
+            # Add a substation point to the start of the PolyLine if it is the first line
+            if i == 0:
+                start_point = folium.CircleMarker(
+                    location=locations[0],  # Start point coordinates
+                    radius=3,
+                    color='green',
+                    fill=True,
+                    fill_color='green',
+                    fill_opacity=0.7,
+                    tooltip=sub1
+                )
+                start_point.add_to(m)
 
-        # Add a substation point at the end of the PolyLine if it is the last line
-        if i == (len(linestrings.geoms) - 1):
-            end_point = folium.CircleMarker(
-                location=locations[-1],  # End point coordinates
-                radius=3,
-                color='green',
-                fill=True,
-                fill_color='green',
-                fill_opacity=0.7,
-                tooltip=sub2
-            )
-            end_point.add_to(m)
+            # Add a substation point at the end of the PolyLine if it is the last line
+            if i == (len(linestrings.geoms) - 1) and len(locations) > 0 and len(locations[-1]) == 2:
+                end_point = folium.CircleMarker(
+                    location=locations[-1],  # End point coordinates
+                    radius=2,
+                    color='green',
+                    fill=True,
+                    fill_color='green',
+                    fill_opacity=0.7,
+                )
+                end_point.add_to(m)
 
 st_data = st_folium(m, width=725)
